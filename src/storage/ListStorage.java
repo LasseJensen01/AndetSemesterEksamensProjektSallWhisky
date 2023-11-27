bb3a96832f98c508d4f6432f6f2818111dad5463
eaashlo <eaashlo@students.eaaa.dk>
2023-11-27 10:55:24 +0100

    Set up storage

--- src/storage/ListStorage.java
+++ src/storage/ListStorage.java
@@ -1,9 +1,34 @@
 package storage;
 
 import controller.Storage;
+import model.Cask;
+import model.NewMake;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListStorage implements Storage, Serializable {
+        private final List<Cask> casks = new ArrayList<>();
+        private final List<NewMake> newMakes = new ArrayList<>();
 
+        @Override
+        public List<Cask> getCasks() {
+                return new ArrayList<>(casks);
+        }
+
+        @Override
+        public void storeCask(Cask cask) {
+                casks.add(cask);
+        }
+
+        @Override
+        public List<NewMake> getNewMakes() {
+                return new ArrayList<>(newMakes);
+        }
+
+        @Override
+        public void storeNewMakes(NewMake newMake) {
+                newMakes.add(newMake);
+        }
 }
