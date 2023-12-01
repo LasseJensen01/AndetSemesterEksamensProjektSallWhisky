ba8b2628b29118bd38ede37004efa2c0c1a3712c
Lasse Jensen <eaa23laje@students.eaaa.dk>
2023-12-01 09:31:04 +0100

    Fixed Warehouse

--- src/model/Warehouse.java
+++ src/model/Warehouse.java
@@ -7,17 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Warehouse {
-    private int id;
-    private static int no = 1;
     private String name;
     private String adress;
     private List<Location> lager = new ArrayList<>();
 
     public Warehouse(String name, String adress) {
-        this.id = no;
         this.name = name;
         this.adress = adress;
-        no++;
     }
 
     public void extractOverview(){
