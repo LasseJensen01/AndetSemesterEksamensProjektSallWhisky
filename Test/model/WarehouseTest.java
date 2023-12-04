b0f7be7173acfb576ffb927c47bfabdc99e11790
Lasse Jensen <eaa23laje@students.eaaa.dk>
2023-12-04 08:58:36 +0100

    test for warehouseExtractOverview done

--- Test/model/WarehouseTest.java
+++ Test/model/WarehouseTest.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import storage.ListStorage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WarehouseTest {
     @BeforeEach
     void setUpEmptyStorage(){
@@ -15,6 +22,48 @@ public class WarehouseTest {
 
     @Test
     void warehouseExtractOverview(){
+        //Clear file WarehouseOverview.txt
+       try{
+           File file = new File("src\\model\\WarehouseOverveiw.txt");
+           file.delete();
+       }catch (Exception e){
 
+       }
+       //Setup
+        Storage listStorage = new ListStorage();
+        Controller.setStorage(listStorage);
+
+        Farmer f = new Farmer("Frede", "Sønderhøj");
+
+        Field field = new Field("Mark",f);
+
+        MaltBatch mb = new MaltBatch("Tobak","NordFyn","Corn",field);
+
+        NewMake nm1 = new NewMake("GoodShit", LocalDate.now(),"LJ",mb);
+
+        Cask cask1 = new Cask(Type.VIRGIN_OAK, 30);
+
+        Filling fill1 = new Filling(cask1,"LJ");
+
+        Amount a = new Amount(nm1,30);
+
+        Controller.addAmountToFilling(fill1,a);
+
+        Warehouse wh = new Warehouse("Sall","Sall");
+        Controller.createLocationsInWarehouse(wh,10,6,3,3);
+
+        wh.getLocations().get(0).setCask(cask1);
+
+        wh.extractOverview();
+
+        //Test
+        try{
+            File file = new File("src\\model\\WarehouseOverveiw.txt");
+            assertTrue(file.isFile());
+            assertTrue(file.exists());
+
+        }catch (Exception e){
+            System.err.println("Error when testing Warehouse extractOverview: " + e.getMessage());
+        }
     }
 }
