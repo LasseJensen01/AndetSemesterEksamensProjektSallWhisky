10f02671210798d01b2debf1041f3bb82f875424
eaashlo <eaashlo@students.eaaa.dk>
2023-12-05 18:26:26 +0100

    .

--- src/gui/WhiskyTabController.java
+++ src/gui/WhiskyTabController.java
@@ -1,10 +1,18 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
 public class WhiskyTabController {
+
     @FXML
     private Button btnBottleWhisky;
 
@@ -20,5 +28,14 @@ public class WhiskyTabController {
     @FXML
     private TextArea txfWhiskyInfo;
 
-
+    @FXML
+    private void registerWhiskeyAction() throws Exception{
+        Parent root = FXMLLoader.load(Gui.getFXMLWhiskyRegistration());
+        Stage stage = new Stage();
+        stage.setMinWidth(root.minWidth(-1));
+        stage.setMinHeight(root.minHeight(-1));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
