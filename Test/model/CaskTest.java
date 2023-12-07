c9cb178696568578cd38f20a59ae3f730e208fe8
eaashlo <eaashlo@students.eaaa.dk>
2023-12-07 14:04:08 +0100

    .Unit-test of the Cask class. Also updated JAVAdocs for the class.

--- Test/model/CaskTest.java
+++ Test/model/CaskTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -7,48 +9,296 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CaskTest {
+    private Farmer farmer;
+    private Field field;
+    private MaltBatch maltBatch;
+    private NewMake newMake;
+    private Cask cask;
+    private Filling filling;
+    private Amount amount;
+    @BeforeEach
+    void setup(){
+        Cask.setNo(0);
+
+        farmer = new Farmer("Lars T", "Hvor kragerne vender.");
+        field = new Field("By the river.", farmer);
+        maltBatch = new MaltBatch("North Jutland","Byg", field);
+        newMake = new NewMake(LocalDate.now(), "Jonas", maltBatch);
+
+        cask = new Cask(Type.TOKAY, 200, "Supplier");
+        cask.setLocation(null);
+        filling = new Filling(cask, "Thomas");
+        amount = new Amount(newMake, 100);
+        filling.addAmount(amount);
+    }
+    @Test
+    void setLiters() {
+        // TC1
+        // Arrange
+        cask.setLiters(80);
+
+        // Act
+        double newLiters = cask.getLiters();
+
+        // Assert
+        assertEquals(80, newLiters);
+    }
 
     @Test
-    void getContentsInfo() {
-        // Case 1: The casket is empty because no filling has been added.
+    void setLocation() {
+        // TC1
         // Arrange
-        Cask cask = new Cask(Type.AMARONE, 50,"Big Barrel");
+        Location location = new Location("1-1-1-1");
+        cask.setLocation(location);
 
         // Act
-        String message = cask.getContentsInfo();
+        Location caskLocation = cask.getLocation();
 
         // Assert
-        assertTrue(message.equals("The cask is empty."));
+        assertEquals(location, caskLocation);
+    }
 
-        // Case 2: The casket is empty because filling.liters == 0.
+    @Test
+    void getType() {
+        // TC1
         // Arrange
-        Filling filling = new Filling(cask, "Jesper");
+        // Only basic data used.
 
         // Act
-        message = cask.getContentsInfo();
+        Type type = cask.getType();
 
         // Assert
-        assertTrue(message.equals("The cask is empty."));
+        assertEquals(Type.TOKAY, type);
+    }
 
-        // Case 3: The casket is not empty.
+    @Test
+    void getVolume() {
+        // TC1
         // Arrange
-        Farmer farmer = new Farmer("Lars T", "Hvor kragerne vender.");
-        Field field = new Field("By the river.", farmer);
-        MaltBatch maltBatch = new MaltBatch("Søren Ryge","Barley", field);
-        NewMake newMake77 = new NewMake(LocalDate.now(), "Jonas", maltBatch);
-        newMake77.setAlcPercent(0.80);
-        Amount amount = new Amount(newMake77, 15);
-        filling.addAmount(amount);
+        // Only basic data used.
 
         // Act
-        message = cask.getContentsInfo();
+        double volume = cask.getVolume();
 
         // Assert
-        assertFalse(message.equals("The cask is empty."));
+        assertEquals(200, volume);
+    }
+
+    @Test
+    void getLiters() {
+        // TC1
+        // Arrange
+        // Only basic data used.
+
+        // Act
+        double liters = cask.getLiters();
+
+        // Assert
+        assertEquals(100, liters);
+    }
+
+    @Test
+    void setFilling() {
+        // TC1
+        // Arrange
+        Cask newCask = new Cask(Type.AMARONE,200,"Supplier");
+
+        // Act
+        Filling newFilling = new Filling(newCask,"James");
+        cask.setFilling(newFilling);
+
+        // Assert
+        assertEquals(newFilling, cask.getFilling());
+    }
+
+    @Test
+    void getFilling() {
+        // TC1
+        // Arrange
+        // Only basic data used.
+
+        // Act & Assert
+        assertEquals(this.filling, cask.getFilling());
+    }
+
+    @Test
+    void getTimesUsed() {
+        // TC1
+        // Arrange
+        cask.emptyCask();
+
+        // Act
+        int timesUsed = cask.getTimesUsed();
+
+        // Assert
+        assertEquals(1, timesUsed);
+
+        // TC2
+        // Arrange
+        cask = new Cask(Type.BORDEAUX,200,"Supplier");
+
+        // Act
+        timesUsed = cask.getTimesUsed();
+
+        // Assert
+        assertEquals(0, timesUsed);
+    }
+
+    @Test
+    void getId() {
+        // TC1
+        // Arrange
+        // Only basis data used
+
+        // Act
+        int id = cask.getId();
+
+        // Assert
+        assertEquals(1, id);
+    }
+
+    @Test
+    void getLocation() {
+        // TC1
+        // Arrange
+        // Only basic data used.
+
+        // Act
+        Location caskLocationTC1 = cask.getLocation();
+
+        // Assert
+        assertEquals(null, caskLocationTC1);
+
+        // TC2
+        // Arrange
+        Location location = new Location("1-1-1-1");
+        cask.setLocation(location);
+
+        // Act
+        Location caskLocationTC2 = cask.getLocation();
+
+        // Assert
+        assertEquals(location, caskLocationTC2);
+    }
+
+    @Test
+    void setNo() {
+        // TC1
+        // Arrange
+        Cask.setNo(100);
+
+        // Act
+        int no = Cask.getNo();
+
+        // Assert
+        assertEquals(100, no);
+    }
+    @Test
+    void getNo() {
+        // TC1
+        // Arrange
+        // Only basic data used.
+
+        // Act
+        int no = Cask.getNo();
+
+        // Assert
+        assertEquals(1, no);
+
+
+        // TC2
+        // Arrange
+        cask = new Cask(Type.BORDEAUX,200,"Supplier");
+
+        // Act
+        no = Cask.getNo();
+
+        // Assert
+        assertEquals(2, no);
+    }
+
+    @Test
+    void testToString() {
+        // TC1
+        // Arrange
+        String expected = "Cask with ID: 1 Type: TOKAY Volume: 200.0";
+
+        // Act
+        String toString = cask.toString();
+
+        // Assert
+        assertTrue(expected.equals(toString));
     }
 
     @Test
     void emptyCask() {
+        // TC1
+        // Arrange
+        Location location = new Location("1-1-1-1");
+        cask.setLocation(location);
+        cask.emptyCask();
+
+        // Act
+        Location caskLokation = cask.getLocation();
+        double litersInCask = cask.getLiters();
+        Filling caskFill = cask.getFilling();
+
+        // Assert
+        assertNull(caskLokation);
+        assertNull(caskFill);
+        assertEquals(0, litersInCask);
+
+    }
+
+    @Test
+    void containsWhisky() {
+        // TC1
+        // Arrange
+        // Only basic data used here
+
+        // Act
+        boolean containsWhisky = cask.containsWhisky();
+
+        // Assert
+        assertFalse(containsWhisky);
+
+        // TC2
+        // Arrange
+        filling.setDate(LocalDate.now().minusYears(3).plusDays(1));
+
+        // Act
+        containsWhisky = cask.containsWhisky();
+
+        // Assert
+        assertFalse(containsWhisky);
+
+        // TC3
+        // Arrange
+        filling.setDate(LocalDate.now().minusYears(3));
+
+        // Act
+        containsWhisky = cask.containsWhisky();
+
+        // Assert
+        assertTrue(containsWhisky);
+
+        // TC4
+        // Arrange
+        filling.setDate(LocalDate.now().minusYears(3).minusDays(1));
+
+        // Act
+        containsWhisky = cask.containsWhisky();
+
+        // Assert
+        assertTrue(containsWhisky);
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+    /*
+    * Tests belonging to an older iteration.
+    * */
+    @Test
+    void iteration7EmptyCask() {
         // Arrange
         Cask cask = new Cask(Type.AMARONE, 50, "Big Barrel");
         Filling filling = new Filling(cask, "Jesper");
@@ -67,7 +317,7 @@ class CaskTest {
     }
 
     @Test
-    void containsWhisky() {
+    void iteration7ContainsWhisky() {
         // Case 1: Boundary value of 3 years
         // Arrange
         Cask cask = new Cask(Type.BORDEAUX, 125, "Big Barrel");
--- src/model/Cask.java
+++ src/model/Cask.java
@@ -10,36 +10,43 @@ public class Cask implements Serializable {
     private Type type;
     private static int no = 0;
     private int id;
-    private double volume; // How big cask
+    private double volume;
     private Filling filling = null;
     private int timesUsed = 0;
     private Location location = null;
-
+    /**
+     * This method creates a cask.
+     * @param type the type of the cask.
+     * @param volume how many liters the cask can contain.
+     * @param supplier the name of the supplier.
+     */
     public Cask(Type type, double volume, String supplier) {
         this.type = type;
         this.volume = volume;
         no++;
         this.id = no;
     }
-    public String getContentsInfo(){
-        String s = "";
-        if (filling == null || filling.getLiters() == 0) s += "The cask is empty.";
-        else {
-            s = "Casket " + id + " contains the following:\n";
-            s += filling.getContentsInfo(LocalDate.now());
-        }
-        return s;
-    }
+    /**
+     * This method empties a cask, and sets it filling, liters and location to null.
+     */
     public void emptyCask(){
         filling.setLiters(0);
         this.filling = null;
-        //this.location.setCask(null);
+        if (this.location != null) this.location.setCask(null);
         this.location = null;
     }
+    /**
+     * Calls the fillings isWisky() method to check if the filling is 3 or more years old.
+     * @return false if filling is null.
+     */
     public boolean containsWhisky(){
+        if (this.filling == null) return false;
         return filling.isWisky();
     }
     //-----------------------------------------------------------------------
+    /**
+     * Sets the amount of filling in the cask. Used incase of spills or evaporation.
+     */
     public void setLiters(double liters){filling.setLiters(liters);}
     public void setLocation(Location location) {
         this.location = location;
@@ -51,11 +58,16 @@ public class Cask implements Serializable {
     public double getVolume() {
         return volume;
     }
-
+    /**
+     * @return the liters of fillling in the cask.
+     */
     public double getLiters() {
         if (filling != null) return filling.getLiters();
         return 0;
     }
+    /**
+     * @param filling a new filling, pre not null.
+     */
     public void setFilling(Filling filling) {
         this.filling = filling;
         this.timesUsed++;
@@ -72,20 +84,27 @@ public class Cask implements Serializable {
     public int getId() {
         return id;
     }
-    public void setTimesUsed(int timesUsed) {
-        this.timesUsed = timesUsed;
-    }
 
     public Location getLocation() {
         return location;
     }
+    /**
+     * @param no the no variable of a class using the Id-tracker. Pre only positive integers.
+     */
     public static void setNo(int no) {
         Cask.no = no;
     }
 
-
+    /**
+     * @returns a string with the format:
+     * "Cask with ID: " + this.id + " Type: " + this.type + " Volume: " + this.volume
+     */
     @Override
     public String toString() {
         return "Cask with ID: " + id + " Type: " + type + " Volume: " + volume;
+    }
+
+    public static int getNo() {
+        return no;
     }
 }
