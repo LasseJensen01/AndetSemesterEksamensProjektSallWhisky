3ebe0e0068adb390d033f4d78c0cf7688fd1364f
Lasse Jensen <eaa23laje@students.eaaa.dk>
2023-12-08 11:33:39 +0100

    NewMakeTabController is fine as fuck

--- src/gui/NewMakeTabController.java
+++ src/gui/NewMakeTabController.java
@@ -127,6 +127,12 @@ public class NewMakeTabController {
             lvwNewMakeinProgress.getItems().remove(newMake);
             lblErrorFinish.setText("");
             lblConfirmation.setText(newMake.toString() + "is registered");
+
+            datePickerEnd.getEditor().clear();
+            txfProducedAmount.clear();
+            txfAlcoholPercent.clear();
+            txaComment.clear();
+            lblConfirmation.setText("");
         }catch (Exception e){
             lblConfirmation.setText("");
             lblErrorFinish.setText("Error: Check data");
